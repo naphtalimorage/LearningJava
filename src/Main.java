bd7bba030264c62fcf7008a2c9a2cd995680a28b
naphtalimorage <naphtalimorage56@gmail.com>
2024-01-20 13:31:46 +0300

    switch case

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,36 @@
 import java.util.Scanner;
 public class Main {
-    static void checkAge(int age) {
-        if(age < 18) {
-            System.out.print("Sorry you are not eligible to vote!");
-        }
-        else{
-            System.out.print("Congratulation! You are eligible to vote");
-        }
-    }
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        System.out.print("Enter your Name: ");
-        String name = myObj.nextLine();
-        System.out.print("Enter your Age: ");
-        int age = myObj.nextInt();
-        System.out.print("Enter your ID: ");
-        int id = myObj.nextInt();
-        checkAge(age);
+        int fnum,pnum,operator;
+        System.out.println("\tOPTIONS");
+        System.out.println("1.Multiplication");
+        System.out.println("2.Subtraction");
+        System.out.println("3.Addition");
+        System.out.println("4.Division\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Option: ");
+        operator = sc.nextInt();
+        System.out.println("Enter your first number: ");
+        fnum = sc.nextInt();
+        System.out.println("Enter your second number: ");
+        pnum = sc.nextInt();
+        double answer = 0;
+        switch (operator) {
+            case 1:
+                answer = fnum * pnum;
+                break;
+            case 2:
+                answer = fnum - pnum;
+                break;
+            case 3:
+                answer = fnum + pnum;
+                break;
+            case 4:
+                answer = fnum / pnum;
+                break;
+            default:
+                System.out.println("Entered number is invalid");
+        }
+        System.out.println("Your answer is: " + answer);
     }
 }
